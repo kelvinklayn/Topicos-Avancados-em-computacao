fc8bd655b7035561408bfc1e41ba1b8a4edc2b19
Kelvin Klayn <kelvin.klayn94@gmail.com>
2023-11-30 19:14:55 -0400

    criacao de controllers e configuracao sequilize

--- Mocaliza/src/main.js
+++ Mocaliza/src/main.js
@@ -1,14 +1,86 @@
-    import express from 'express';
-    const app = express();
-    const port = 8000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
-    app.get('/',(req, res) =>{
-        res.json({
-            data: Date.now()
-        });
-    });
+const { Database } = require('./configs/sequelize');
+const { CategoryController } = require('./controllers/category-controller.js');
 
-    app.listen(port, () => {
-        console.log(`O servidor foi iniciado na porta ${port}!`);
-    });
+const db = Database.getInstance();
 
+const app = express();
+const categoryController = new CategoryController();
+
+app.use(bodyParser.json());
+
+app.get('/api/categories', async (req, res) => {
+	const categories = await categoryController.index();
+	
+	res.json({
+		data: categories
+	});
+});
+
+app.get('/api/categories/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	const category = await categoryController.show(id);
+	
+	res.json({
+		data: category,
+	});
+});
+
+app.post('/api/categories', async (req, res) => {
+	const categoryDto = {
+		name: req.body.name,
+		active: true,
+	};
+	
+	const category = await categoryController.store(categoryDto);
+	
+	res.status(201);
+	res.json({
+		data: category
+	});
+});
+
+app.delete('/api/categories/:id', async (req, res) => {
+	const id = parseInt(req.param.id);
+	
+	try {
+		await categoryController.destroy(id);
+		
+		res.status(204);
+		res.end();
+	} catch (e) {
+		res.status(404);
+		res.json({
+			error: e,
+		});
+	}
+});
+
+app.put('/api/categories/:id', async (req, res) => {
+	const categoryDto = {
+		name: req.body.name,
+		active: req.body.active,
+	};
+	
+	try {
+		const category = await categoryController.update(id, categoryDto);
+		
+		res.json({
+			data: category,
+		});
+	} catch (e) {
+		res.status(404);
+		res.json({
+			error: e,
+		});
+	}
+});
+
+app.listen(8000, async () => {
+	
+	
+	// await db.sync();
+	console.log('Server is running on port 8000!');
+});
